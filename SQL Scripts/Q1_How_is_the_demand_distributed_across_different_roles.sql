00bfe6d66a83af8e2b2693964c3a4fceabc0cd5a
Maxhajati <m.hajati@gmail.com>
2025-03-06 21:17:58 -0400

    Update Q1_How_is_the_demand_distributed_across_different_roles.sql

--- SQL Scripts/Q1_How_is_the_demand_distributed_across_different_roles.sql
+++ SQL Scripts/Q1_How_is_the_demand_distributed_across_different_roles.sql
@@ -13,19 +13,58 @@ SELECT
     job_title, 
     COUNT(*) AS job_count,
     ROUND((COUNT(*) * 100.0 / 2385), 2) || '%' AS role_percentage
-FROM job_posting_fact
-GROUP BY job_title
-ORDER BY job_count DESC;
+FROM 
+    job_posting_fact
+GROUP BY 
+    job_title
+ORDER BY 
+    job_count DESC;
 
 
 /* Insights
-    The data is pulled from the job_posting_fact table, which contains job posting information. 
-    The results are grouped by job_title so that the COUNT and percentage are calculated for each role.
+
+Here's the breakdown of the distribution of job postings across various job titles within the dataset:
+
+- Business Analysts lead with 36.3%, emphasizing their key role in bridging business and tech.
+- Data Analysts follow at 29.5%, reflecting the high need for data gathering and analysis.
+- Data Engineers are in demand at 10.9%, highlighting the need for experts in data architecture.
+- Data Scientists and Cloud Engineers each hold 9.1%, showcasing the importance of analytics and cloud infrastructure.
+- ML Engineers account for 5.2%, showing the demand for AI model developers.
+
+=======
+RESULTS
+=======
+
+[
+  {
+    "job_title": "Business Analyst",
+    "job_count": 866,
+    "job_percentage": "36.31%"
+  },
+  {
+    "job_title": "Data Analyst",
+    "job_count": 703,
+    "job_percentage": "29.48%"
+  },
+  {
+    "job_title": "Data Engineer",
+    "job_count": 260,
+    "job_percentage": "10.90%"
+  },
+  {
+    "job_title": "Cloud Engineer",
+    "job_count": 217,
+    "job_percentage": "9.10%"
+  },
+  {
+    "job_title": "Data Scientist",
+    "job_count": 216,
+    "job_percentage": "9.06%"
+  },
+  {
+    "job_title": "ML Engineer",
+    "job_count": 123,
+    "job_percentage": "5.16%"
+  }
+]
 */
-
-
-
-
-
-
-
