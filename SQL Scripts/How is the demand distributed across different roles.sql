522c86f35f197a411ce124a947d3874d766726e8
Maxhajati <m.hajati@gmail.com>
2025-03-06 20:45:06 -0400

    Update How is the demand distributed across different roles.sql

--- SQL Scripts/How is the demand distributed across different roles.sql
+++ SQL Scripts/How is the demand distributed across different roles.sql
@@ -1,1 +1,7 @@
-
+SELECT 
+    job_title, 
+    COUNT(*) AS role_count,
+    ROUND((COUNT(*) * 100.0 / 2385), 2) || '%' AS role_percentage
+FROM job_posting_fact
+GROUP BY job_title
+ORDER BY role_count DESC;
