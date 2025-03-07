7088dc0a7165a24fc179448801c1405b3fdee324
Maxhajati <m.hajati@gmail.com>
2025-03-06 20:59:38 -0400

    Update Q1_How_is_the_demand_distributed_across_different_roles.sql

--- SQL Scripts/Q1_How_is_the_demand_distributed_across_different_roles.sql
+++ SQL Scripts/Q1_How_is_the_demand_distributed_across_different_roles.sql
@@ -9,38 +9,23 @@ helps to understand the relative demand for different job titles and their impor
 */
 
 
-/* 
+SELECT 
+    job_title, 
+    COUNT(*) AS job_count,
+    ROUND((COUNT(*) * 100.0 / 2385), 2) || '%' AS role_percentage
+FROM job_posting_fact
+GROUP BY job_title
+ORDER BY job_count DESC;
+
+
+/* Insights
     The data is pulled from the job_posting_fact table, which contains job posting information. 
     The results are grouped by job_title so that the COUNT and percentage are calculated for each role.
 */
 
 
 
-SELECT * FROM employees;
 
 
 
 
-SELECT 
-    -- Select the job title from the table to categorize job postings
-    job_title, 
-    
-    -- Count the total number of job postings for each job title
-    -- COUNT(*) counts the number of rows for each job_title
-    COUNT(*) AS role_count,
-    
-    -- Calculate the percentage of job postings for each job title out of the total (2385)
-    -- ROUND() function is used to round the result to 2 decimal places
-    -- (COUNT(*) * 100.0 / 2385) calculates the percentage of job postings
-    -- || '%' concatenates the percentage symbol to the result
-    ROUND((COUNT(*) * 100.0 / 2385), 2) || '%' AS role_percentage
-    
--- From the job_posting_fact table, which contains the job posting data
-FROM job_posting_fact
-
--- Group the results by job_title so that the COUNT and percentage are calculated for each role
-GROUP BY job_title
-
--- Order the results in descending order based on role_count
--- This ensures that job titles with more postings appear at the top
-ORDER BY role_count DESC;
