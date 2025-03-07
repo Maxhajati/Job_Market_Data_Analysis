a6b2d59a44fa8bf6bb2b534e8ce21320797d2ac8
Maxhajati <m.hajati@gmail.com>
2025-03-06 21:33:18 -0400

    demand_distributed_across_different_roles.sql
    demand_distributed_across_different_roles.sql

--- SQL Scripts/Insight_1_demand_distributed_across_different_roles.sql
+++ SQL Scripts/Insight_1_demand_distributed_across_different_roles.sql
@@ -12,7 +12,7 @@ helps to understand the relative demand for different job titles and their impor
 SELECT 
     job_title, 
     COUNT(*) AS job_count,
-    ROUND((COUNT(*) * 100.0 / 2385), 2) || '%' AS role_percentage
+    ROUND((COUNT(*) * 100.0 / 2385), 2) || '%' AS role_percentage 
 FROM 
     job_posting_fact
 GROUP BY 
